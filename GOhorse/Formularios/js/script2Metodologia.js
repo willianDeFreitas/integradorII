27d8953bc290cd205ab3d45220860839a9f6376b
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2017-11-08 20:42:32 -0200

    UserSenha e Gohorse

---
+++ GOhorse/Formularios/js/script2Metodologia.js
@@ -0,0 +1,33 @@
+// <script language="javascript" type="text/javascript">
+function validar(){
+	var pontos = 0;
+	var mensagem="";
+	var q1 = form2.QAOC1.value;
+	var q2 = form2.QAOC2.value;
+	var q3 = form2.QAOC3.value;
+	var q4 = form2.QAOC4.value;
+	var q5 = form2.QAOC5.value;
+	if(q1=="d"){
+		pontos++;
+		mensagem+= "\nAcertou a Questão 1.";
+	}
+	if(q2=="b"){
+		mensagem+= "\nAcertou a Questão 2."
+		pontos++;
+	}
+	if(q3=="c"){
+		mensagem+= "\nAcertou a Questão 3."
+		pontos++;
+	}
+	if(q4=="a"){
+		mensagem+= "\nAcertou a Questão 4."
+		pontos++;
+	}
+	if(q5=="d"){
+		mensagem+= "\nAcertou a Questão 5."
+		pontos++;
+	}
+	alert("Acertos: "+pontos+ mensagem);
+	
+}	
+
